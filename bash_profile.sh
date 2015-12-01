1d6ccbdd82d847ffa86bce988dbdde594b9db304
pabloqc <pablo.qc@gmail.com>
2015-12-01 16:22:46 +0100

    first commit

---
+++ bash_profile.sh
@@ -0,0 +1,38 @@
+GIT_PS1_SHOWUPSTREAM="auto"
+GIT_PS1_SHOWCOLORHINTS="yes"
+source ~/.git-prompt.sh
+
+export PROMPT_COMMAND='__git_ps1 "\u@\h:\W" "\\\$ ";'
+
+#it loads rbenv every time you open a terminal
+if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
+
+# Easier navigation: .., ..., ...., ....., ~ and -
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+alias c='clear'
+
+# Shortcuts
+alias repo="cd ~/Repositories"
+
+# Detect which `ls` flavor is in use
+if ls --color > /dev/null 2>&1; then # GNU `ls`
+        colorflag="--color"
+else # OS X `ls`
+        colorflag="-G"
+fi
+
+# List all files colorized in long format
+alias l="ls -lh ${colorflag}"
+
+# List all files colorized in long format, including dot files
+alias la="ls -laF ${colorflag}"
+
+# List only directories
+alias lsd="ls -lF ${colorflag} | grep --color=never '^d'"
+
+# Always use color output for `ls`
+alias ls="command ls ${colorflag}"
+
---
+++ brew.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Ask for the administrator password upfront.
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade --all
+
+# Install GNU core utilities (those that come with OS X are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
+
+
+brew install bash
+brew tap homebrew/versions
+brew install bash-completion2
+
+# Install `wget` with IRI support.
+brew install wget --with-iri
+
+# Install more recent versions of some OS X tools.
+brew install vim --override-system-vi
+brew install homebrew/dupes/grep
+brew install homebrew/dupes/openssh
+brew install homebrew/dupes/screen
+
+#Install Java
+brew tap caskroom/cask
+brew install brew-cask
+brew cask install java
+
+#Install Gradle
+brew install gradle
+
+#Extras: Python3 and AWS CLI
+brew install python3
+easy_install pip
+pip install awscli
+
+# Remove outdated versions from the cellar.
+brew cleanup
